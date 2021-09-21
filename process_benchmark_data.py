ee1aa023a4fb21e0c00125ef9c98c923b152e43d
benef23 <benedikt.feldotto@gmail.com>
2021-09-21 15:53:57 +0200

    Add benchmark processing template file

---
+++ process_benchmark_data.py
@@ -0,0 +1,20 @@
+
+
+
+
+class ProcessBenchmarkData:
+
+    def __init__(self):
+        # TODO create new results folder
+
+
+    def print_network_creation_time(self):
+        # Benedikt
+
+
+    def create_memory_plot(self):
+        # Jochen
+
+
+    def create_profiler_runtime_plot(self):
+        # Cristian
