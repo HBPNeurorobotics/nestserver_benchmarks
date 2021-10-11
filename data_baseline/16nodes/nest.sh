0ac688882d13a93c33bb08e757ab260d5e5770b3
Jochen Martin Eppler <j.eppler@fz-juelich.de>
2021-10-11 19:43:24 +0200

    Add baseline data and rename result files for exiting data

---
+++ data_baseline/16nodes/nest.sh
@@ -0,0 +1,12 @@
+
+module load sarus
+
+srun --jobid=34330167 -C mc -A ich004m --mpi=pmi2 -v \
+     --cpu-bind=threads --distribution=block:cyclic:fcyclic \
+     -N 16 -n 32 --exclude=nid01411 \
+     sarus run \
+           --mount=type=bind,source=$HOME,dst=$HOME \
+           christopherbignamini/nest:jougs_nest_server_mpi \
+           bash -c '\
+               source /opt/nest/bin/nest_vars.sh; \
+               nest-server-mpi --host 0.0.0.0 --port 5000'
