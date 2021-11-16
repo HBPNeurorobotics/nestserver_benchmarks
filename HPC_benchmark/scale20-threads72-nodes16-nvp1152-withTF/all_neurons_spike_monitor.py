57aa85c27b7ca1b477cca9aed700a64c5f57a14b
cjimenezromero <c.jimenez-romero@fz-juelich.de>
2021-11-16 12:33:31 +0100

    Modified transfer function using a population of 1000 neurons

--- HPC_benchmark/scale20-threads72-nodes16-nvp1152-withTF/all_neurons_spike_monitor.py
+++ HPC_benchmark/scale20-threads72-nodes16-nvp1152-withTF/all_neurons_spike_monitor.py
@@ -1,6 +1,9 @@
-@nrp.MapSpikeSink("neuron_1", nrp.brain.record[1], nrp.leaky_integrator_alpha)
+import numpy as np
+@nrp.MapSpikeSink("neuron_p_1", nrp.map_neurons(range(0,1000), lambda i: nrp.brain.record[i]), nrp.leaky_integrator_alpha)
 @nrp.NeuronMonitor(nrp.brain.record, nrp.leaky_integrator_alpha)
-def all_neurons_spike_monitor(t, neuron_1):
-    clientLogger.info(neuron_1.voltage)
+def all_neurons_spike_monitor (t, neuron_p_1):
+    all_neuron_mean_voltages = np.mean(neuron_p_1.voltage)
+    clientLogger.info(all_neuron_mean_voltages)
     return True
+
 
