5c4e9cd1d33780d6bfc9b48182c833ca446e7884
Benedikt Feldotto <feldotto@in.tum.fe>
2021-12-02 21:47:09 +0100

    Add data hpcbench with TF 4 repetitions

---
+++ Results/2_nrpexperiment_scale20-threads72-nodes16-nvp1152-withTF/2021-12-01_23-33-07-hpcbench_readspikes/4/16nodes/nest.sh
@@ -0,0 +1,17 @@
+
+module load sarus
+
+srun --jobid=35302130 -C mc -A ich004m --mpi=pmi2 -v \
+     --cpu-bind=threads --distribution=block:cyclic:fcyclic \
+     -N 16 -n 32 --exclude=nid00275 \
+     sarus run \
+           --mount=type=bind,source=$HOME,dst=$HOME \
+           load/library/nest_latest_daint:latest \
+           bash -c '\
+	       ln -s $HOME/bf_data//nestserver_benchmarks/Experiments/RoboBrain_benchmark/1_nrpexperiment_robobrain_mouse/resources /opt/data; \
+	       echo BFFFFFF; \
+	       ls /opt/data; \
+	       export NEST_SERVER_MODULES="nest,numpy,time,math"; \
+               export NEST_SERVER_RESTRICTION_OFF=true; \
+	       source /opt/nest/bin/nest_vars.sh; \
+               nest-server-mpi --host 0.0.0.0 --port 5000'
---
+++ Results/2_nrpexperiment_scale20-threads72-nodes16-nvp1152-withTF/2021-12-01_23-33-07-hpcbench_readspikes/4/nrp.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+
+module load sarus
+
+export NRPLOGDIR=/users/bp000231/bf_data/nestserver_benchmarks/tmp_benchmarks_results/2021-12-01_23-33-07-hpcbench_readspikes/4/nrplogs
+
+mkdir -p $NRPLOGDIR/nginx
+mkdir -p $NRPLOGDIR/nrp-services_app
+mkdir -p $NRPLOGDIR/ros-simulation-factory_app
+mkdir -p $NRPLOGDIR/rosbridge
+mkdir -p $NRPLOGDIR/roscore
+mkdir -p $NRPLOGDIR/rosvideoserver
+mkdir -p $NRPLOGDIR/virtualcoach
+mkdir -p $NRPLOGDIR/uwsgi
+mkdir -p $NRPLOGDIR/nginx_home
+
+export NRPSRC=/home_daint/bbpnrsoa/nrp/src
+export NRPSRCCOMM=$NRPSRC/ExDBackend/hbp_nrp_commons/hbp_nrp_commons
+export NRPSRCSERV=$NRPSRC/ExDBackend/hbp_nrp_cleserver/hbp_nrp_cleserver
+export NRPSRCLNCH=$NRPSRC/BrainSimulation/hbp_nrp_distributed_nest/hbp_nrp_distributed_nest/launch
+export NRPVARLOG=/var/log/supervisor
+
+sarus run \
+      --mount=type=bind,source=/etc/opt/slurm/slurm.conf,dst=/usr/local/etc/slurm.conf \
+      --mount=type=bind,source=/var/run/munge/munge.socket.2,dst=/var/run/munge/munge.socket.2 \
+      --mount=type=bind,source=$HOME,dst=$HOME \
+      --mount=type=bind,source=$NRPLOGDIR/nginx,dst=$NRPVARLOG/nginx \
+      --mount=type=bind,source=$NRPLOGDIR/nrp-services_app,dst=$NRPVARLOG/nrp-services_app \
+      --mount=type=bind,source=$NRPLOGDIR/ros-simulation-factory_app,dst=$NRPVARLOG/ros-simulation-factory_app \
+      --mount=type=bind,source=$NRPLOGDIR/rosbridge,dst=$NRPVARLOG/rosbridge \
+      --mount=type=bind,source=$NRPLOGDIR/roscore,dst=$NRPVARLOG/roscore \
+      --mount=type=bind,source=$NRPLOGDIR/rosvideoserver,dst=$NRPVARLOG/rosvideoserver \
+      --mount=type=bind,source=$NRPLOGDIR/virtualcoach,dst=$NRPVARLOG/virtualcoach \
+      --mount=type=bind,source=$NRPLOGDIR/uwsgi,dst=$NRPVARLOG/uwsgi \
+      --mount=type=bind,source=$NRPLOGDIR/nginx_home,dst=/home_daint/bbpnrsoa/nginx \
+      \
+      nexus.neurorobotics.ebrains.eu/daint/nrp:3.2.0-nest-client \
+      \
+      bash -c "\
+          unset LD_PRELOAD; \
+          export LD_LIBRARY_PATH=/usr/lib:$LD_LIBRARY_PATH; \
+          export LC_ALL=C; unset LANGUAGE; \
+          source $NRPSRC/user-scripts/nrp_variables; \
+	  sed -i 's|self.__remaining_time = timeout|self.__remaining = timeout|' $NRPSRC/ExDBackend/hbp_nrp_cleserver/hbp_nrp_cleserver/server/SimulationServer.py; \
+          sed -i 's|<STORAGE_ADDR>|148.187.148.14|' $NRPSRCCOMM/workspace/Settings.py; \
+          sed -i 's|= MPILauncher|= DaintLauncher|' $NRPSRCLNCH/NestLauncher.py; \
+          sed -i 's|sys.executable|\\\"/home_daint/bbpnrsoa/.opt/platform_venv/bin/python\\\"|' $NRPSRCLNCH/NestLauncher.py; \
+          sed -i 's|$VGLRUN|xvfb-run -a --server-args=\\\"-screen 0 1280x1024x24\\\"|' /home_daint/bbpnrsoa/.opt/bbp/nrp-services/gzserver; \
+          sed -i 's|resolver 8.8.8.8;|resolver 148.187.18.88;|' /home_daint/bbpnrsoa/.local/etc/nginx/conf.d/nrp-services.conf; \
+          sed -i 's|--pause|--pause --software_only_rendering|' /home_daint/bbpnrsoa/.opt/bbp/nrp-services/gzserver; \
+          sed -i 's|NestClientControlAdapter()|NestClientControlAdapter(\"nid00276\", 5000)|' $NRPSRCSERV/server/ServerConfigurations.py;
+          \
+          /etc/init.d/supervisor start;
+          sleep infinity;"
