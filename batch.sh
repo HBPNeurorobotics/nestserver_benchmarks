2f85ad152f909443798508b9201f401e58203ad5
Benedikt Feldotto <feldotto@in.tum.fe>
2021-12-04 23:12:25 +0100

    Data in results subfolder

--- batch.sh
+++ batch.sh
@@ -1,14 +1,18 @@
-config=$1
+configfile=$1
 
-repetitions=$(grep 'repetitions:' $config | cut -d: -f2 | tr -d ' ' | tr -d '"')
-testcase=$(grep 'testcase:' $config | cut -d: -f2 | tr -d ' ' | tr -d '"')
+repetitions=$(grep 'repetitions:' $configfile | cut -d: -f2 | tr -d ' ' | tr -d '"')
+testcase=$(grep 'testcase:' $configfile | cut -d: -f2 | tr -d ' ' | tr -d '"')
 
 DATE_WITH_TIME=`date "+%Y-%m-%d_%H-%M-%S"`
+resultsdir=$PWD/tmp_benchmarks_results/$DATE_WITH_TIME-$testcase/results
+mkdir -p $resultsdir
+
+cp $configfile $resultsdir/config.yaml
 
 for (( c=1; c<=$repetitions; c++ ))
 do
 		
-	RUNDIR=$PWD/tmp_benchmarks_results/$DATE_WITH_TIME-$testcase/$c
+	RUNDIR=$resultsdir/$c
 
 	echo "==== BATCHRUNNER: Starting repetition $c in $RUNDIR ==="
 
@@ -19,5 +23,5 @@ do
 		--ntasks 66 \
 		--cpus-per-task 36 \
 		--hint=multithread \
- 		job.sh $config $RUNDIR
+ 		job.sh $configfile $RUNDIR
 done
