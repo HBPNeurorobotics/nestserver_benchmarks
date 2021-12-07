78eeb4fe3a494f4755d6411a3ab64adf05139c3a
Benedikt Feldotto <feldotto@in.tum.fe>
2021-12-07 18:50:23 +0100

    Fix robot id for muscle publisher

--- Experiments/RoboBrain_benchmark/1_nrpexperiment_robobrain_mouse/b_muscle_controller.py
+++ Experiments/RoboBrain_benchmark/1_nrpexperiment_robobrain_mouse/b_muscle_controller.py
@@ -1,14 +1,14 @@
 from std_msgs.msg import Float64
 @nrp.MapVariable("muscle_actuation", initial_value=8*[0.], scope=nrp.GLOBAL)
 
-@nrp.MapRobotPublisher("Foot1", Topic("/gazebo_muscle_interface/robobrain_mouse/Foot1/cmd_activation", Float64))
-@nrp.MapRobotPublisher("Foot2", Topic("/gazebo_muscle_interface/robobrain_mouse/Foot2/cmd_activation", Float64))
-@nrp.MapRobotPublisher("Radius1", Topic("/gazebo_muscle_interface/robobrain_mouse/Radius1/cmd_activation", Float64))
-@nrp.MapRobotPublisher("Radius2", Topic("/gazebo_muscle_interface/robobrain_mouse/Radius2/cmd_activation", Float64))
-@nrp.MapRobotPublisher("Humerus1", Topic("/gazebo_muscle_interface/robobrain_mouse/Humerus1/cmd_activation", Float64))
-@nrp.MapRobotPublisher("Humerus2", Topic("/gazebo_muscle_interface/robobrain_mouse/Humerus2/cmd_activation", Float64))
-@nrp.MapRobotPublisher("Humerus3", Topic("/gazebo_muscle_interface/robobrain_mouse/Humerus3/cmd_activation", Float64))
-@nrp.MapRobotPublisher("Humerus4", Topic("/gazebo_muscle_interface/robobrain_mouse/Humerus4/cmd_activation", Float64))
+@nrp.MapRobotPublisher("Foot1", Topic("/gazebo_muscle_interface/robobrain_mouse_with_joystick/Foot1/cmd_activation", Float64))
+@nrp.MapRobotPublisher("Foot2", Topic("/gazebo_muscle_interface/robobrain_mouse_with_joystick/Foot2/cmd_activation", Float64))
+@nrp.MapRobotPublisher("Radius1", Topic("/gazebo_muscle_interface/robobrain_mouse_with_joystick/Radius1/cmd_activation", Float64))
+@nrp.MapRobotPublisher("Radius2", Topic("/gazebo_muscle_interface/robobrain_mouse_with_joystick/Radius2/cmd_activation", Float64))
+@nrp.MapRobotPublisher("Humerus1", Topic("/gazebo_muscle_interface/robobrain_mouse_with_joystick/Humerus1/cmd_activation", Float64))
+@nrp.MapRobotPublisher("Humerus2", Topic("/gazebo_muscle_interface/robobrain_mouse_with_joystick/Humerus2/cmd_activation", Float64))
+@nrp.MapRobotPublisher("Humerus3", Topic("/gazebo_muscle_interface/robobrain_mouse_with_joystick/Humerus3/cmd_activation", Float64))
+@nrp.MapRobotPublisher("Humerus4", Topic("/gazebo_muscle_interface/robobrain_mouse_with_joystick/Humerus4/cmd_activation", Float64))
 @nrp.Robot2Neuron()
 def b_muscle_controller (t, muscle_actuation, Foot1, Foot2, Humerus1, Humerus2, Humerus3, Humerus4, Radius1, Radius2):
     
