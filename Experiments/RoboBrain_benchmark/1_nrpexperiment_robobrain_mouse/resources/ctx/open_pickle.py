0634a7622e726692cad5df80d22a07369e6e8a54
Benedikt Feldotto <feldotto@in.tum.fe>
2022-01-19 14:43:53 +0100

    Check for sigma = 0

--- Experiments/RoboBrain_benchmark/1_nrpexperiment_robobrain_mouse/resources/ctx/open_pickle.py
+++ Experiments/RoboBrain_benchmark/1_nrpexperiment_robobrain_mouse/resources/ctx/open_pickle.py
@@ -1,7 +1,22 @@
 import pprint
 import pandas as pd
 
+
 pickle_file = pd.read_pickle(r'S1_internal_connection.pickle')
 
+
+# Print all the Pickle file
 pprint.pprint(pickle_file)
 
+
+# Print all sigma = 0 layers
+for a in pickle_file:
+    print("region: ", a)
+    for b in pickle_file[a]:
+        
+        for c in pickle_file[a][b]:
+            if c == "sigma":
+                if pickle_file[a][b][c] == 0:
+                    
+                    print("  layer:", b)
+                    print("    sigma:", pickle_file[a][b][c])
