3030befbff8849c86a0f9942e4eaabcf9b592b01
Benedikt Feldotto <feldotto@in.tum.fe>
2022-01-21 12:39:17 +0100

    Change sigma from 0 to 0.2 and script to save changes

--- Experiments/RoboBrain_benchmark/1_nrpexperiment_robobrain_mouse/resources/ctx/open_pickle.py
+++ Experiments/RoboBrain_benchmark/1_nrpexperiment_robobrain_mouse/resources/ctx/open_pickle.py
@@ -1,22 +1,37 @@
 import pprint
 import pandas as pd
+import pickle
 
 
-pickle_file = pd.read_pickle(r'S1_internal_connection.pickle')
+file = r'S1_internal_connection.pickle'
+
+pickle_file = []
+with open(file, 'rb+') as pf:
+    pickle_file = pickle.load(pf)
 
 
 # Print all the Pickle file
 pprint.pprint(pickle_file)
 
-
 # Print all sigma = 0 layers
 for a in pickle_file:
     print("region: ", a)
     for b in pickle_file[a]:
-        
+      
         for c in pickle_file[a][b]:
             if c == "sigma":
                 if pickle_file[a][b][c] == 0:
-                    
+                       
                     print("  layer:", b)
                     print("    sigma:", pickle_file[a][b][c])
+                    pickle_file[a][b][c] = 0.2
+                    print(pickle_file[a][b][c])
+
+
+with open(file, 'wb+') as pf:
+    pickle.dump(pickle_file, pf)
+
+
+#df.to_pickle(r'S1_internal_connection.pickle')
+
+
