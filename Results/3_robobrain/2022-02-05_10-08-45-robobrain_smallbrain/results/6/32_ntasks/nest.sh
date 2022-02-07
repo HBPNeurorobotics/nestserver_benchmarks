1e56a92b6db2507ee66c1a4ae7f1a2295f26a5af
Benedikt Feldotto <feldotto@in.tum.fe>
2022-02-07 14:35:46 +0100

    Results of HPC benchmark with out Transfer Functions + Robobrain motor cortex only

---
+++ Results/3_robobrain/2022-02-05_10-08-45-robobrain_smallbrain/results/6/32_ntasks/nest.sh
@@ -0,0 +1,18 @@
+
+module load sarus
+
+srun --jobid=36563689 -C mc -A ich004m --mpi=pmi2 -v \
+     --cpu-bind=threads --distribution=block:cyclic:fcyclic \
+     -N 16 -n 32 --exclude=nid01416 \
+     sarus run \
+           --mount=type=bind,source=$HOME,dst=$HOME \
+           load/library/nest_latest_daint:latest \
+           bash -c ' \
+               source /opt/nest/bin/nest_vars.sh; \
+	       ln -s /users/bp000231/bf_data/nestserver_benchmarks/Experiments/RoboBrain_benchmark/1_nrpexperiment_robobrain_mouse/resources /opt/data; \
+	       cp /users/bp000231/bf_data/nestserver_benchmarks/fixes/hl_api_server.py /opt/nest/lib/python3.8/site-packages/nest/server/hl_api_server.py; \
+	       echo NEST server dir is; \
+	       ls /opt/nest/lib/python3.8/site-packages/nest/server; \
+	       export NEST_SERVER_MODULES="nest,numpy,time,math"; \
+	       export NEST_SERVER_RESTRICTION_OFF=true; \
+               nest-server-mpi --host 0.0.0.0 --port 5000'
